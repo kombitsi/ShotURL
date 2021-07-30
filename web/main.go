a8adb223d9335189a5155994f5c0518a30e8d847
Dmitry Kombitsi <dmitrykombitsi@MacBook-Pro-Dmitry.local>
2021-07-30 13:40:25 +0300

    first commit

--- web/main.go
+++ web/main.go
@@ -1,16 +1,2 @@
 package main
 
-import (
-    "log"
-    "net/http"
-)
-
-
-func main()  {
-    mux :=http.NewServeMux()
-    mux.HandleFunc("/short", longurl)
-    mux.HandleFunc("/long", shoturl)
-    log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
-    err := http.ListenAndServe(":8080", mux)
-    log.Fatal(err)
-}
